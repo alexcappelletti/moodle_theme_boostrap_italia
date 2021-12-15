2088b03a36f7e1da8c5a5a6428bbf73175655f37
alex cappelletti <cappelle@gmail.com>
2021-12-15 16:43:35 +0100

    tema klass con modifiche sullo schema colori. Usa font titillium web caricato da endpoint google. non funziona la parte di footer (da verificare)

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 
 // This line protects the file from being accessed by a URL directly.                                                               
 defined('MOODLE_INTERNAL') || die();
-
+require_once(__DIR__ . '/lib.php');
 // $THEME is defined before this page is included and we can define settings by adding properties to this global object.            
                                                                                                                                     
 // The first setting we need is the name of the theme. This should be the last part of the component name, and the same             
@@ -51,13 +51,121 @@ $THEME->hidefromselector = false;
 
 // This is the function that returns the SCSS source for the main file in our theme. We override the boost version because          
 // we want to allow presets uploaded to our own theme file area to be selected in the preset list.                                  
-$THEME->scss = function($theme) {                                                                                                   
+// $THEME->scss = function($theme) {                                                                                                   
+//     return theme_boost_materialized_get_agid($theme);                                                                               
+// };
+//$THEME->sheets = ['theme' , 'custom'];
+$THEME->javascripts_footer = array('theme');
+//$THEME->csspostprocess = 'theme_boost_materialized_process_css';
+ $THEME->scss = function($theme) {                                                                                                   
     return theme_boost_materialized_get_agid($theme);                                                                               
 };
-
-// $THEME->precompiledcsscallback = function() {
-// 	global $CFG;
-//     return file_get_contents($CFG->dirroot . '/theme/boost_materialized/style/journal.css');
-
-// };
-
+$THEME->layouts = array(
+        // Most backwards compatible layout without the blocks - this is the layout used by default.
+    'base' => array(
+        'file' => 'columns1.php',
+        'regions' => array(),
+    ),
+    // Standard layout with blocks, this is recommended for most pages with general information.
+    'standard' => array(
+        'file' => 'columns2.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    // Main course page.
+    'course' => array(
+        'file' => 'columns2.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+        'options' => array('langmenu' => true),
+    ),
+    'coursecategory' => array(
+        'file' => 'columns2.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    // The site home page.
+    'frontpage' => array(
+        'file' => 'frontpage.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+        'options' => array('nonavbar' => true),
+    ),
+    // Server administration scripts.
+    'admin' => array(
+        'file' => 'columns2.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    'login' => array(
+        'file' => 'login.php',
+        'regions' => array(),
+        'options' => array('langmenu' => true),
+    ),
+    // Pages that appear in pop-up windows - no navigation, no blocks, no header.
+    'popup' => array(
+        'file' => 'columns1.php',
+        'regions' => array(),
+        'options' => array('nofooter' => true, 'nonavbar' => true),
+    ),
+    // My dashboard page.
+    'mydashboard' => array(
+        'file' => 'columns2.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+        'options' => array('nonavbar' => true, 'langmenu' => true, 'nocontextheader' => true),
+    ),
+    // Part of course, typical for modules - default page layout if $cm specified in require_login().
+    'incourse' => array(
+        'file' => 'columns2.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    // No blocks and minimal footer - used for legacy frame layouts only!
+    'frametop' => array(
+        'file' => 'columns1.php',
+        'regions' => array(),
+        'options' => array('nofooter' => true, 'nocoursefooter' => true),
+    ),
+    // Embeded pages, like iframe/object embeded in moodleform - it needs as much space as possible.
+    'embedded' => array(
+        'file' => 'embedded.php',
+        'regions' => array()
+    ),
+    // Used during upgrade and install, and for the 'This site is undergoing maintenance' message.
+    // This must not have any blocks, links, or API calls that would lead to database or cache interaction.
+    // Please be extremely careful if you are modifying this layout.
+    'maintenance' => array(
+        'file' => 'maintenance.php',
+        'regions' => array(),
+    ),
+    // Should display the content and basic headers only.
+    'print' => array(
+        'file' => 'columns1.php',
+        'regions' => array(),
+        'options' => array('nofooter' => true, 'nonavbar' => false),
+    ),
+    // The pagelayout used when a redirection is occuring.
+    'redirect' => array(
+        'file' => 'embedded.php',
+        'regions' => array(),
+    ),
+    // The pagelayout used for reports.
+    'report' => array(
+        'file' => 'columns2.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    // My public page.
+    'mypublic' => array(
+        'file' => 'columns2.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    // The pagelayout used for safebrowser and securewindow.
+    'secure' => array(
+        'file' => 'secure.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre'
+    )
+);
--- settings.php
+++ settings.php
@@ -76,5 +76,227 @@ if ($ADMIN->fulltree) {
     $setting->set_updatedcallback('theme_reset_all_caches');                                                                        
     $page->add($setting);                                                                                                           
                                                                                                                                     
-    $settings->add($page);                                                                                                          
+    $settings->add($page);                        
 }
+// if (is_siteadmin()) {
+
+//     $settings = new theme_boost_admin_settingspage_tabs('themesettingboost_materialized', get_string('configtitle', 'theme_boost_materialized'));
+//     $ADMIN->add('themes', new admin_category('theme_boost_materialized', 'boost_materialized'));
+
+//     /* Header Settings */
+//     $temp = new admin_settingpage('theme_boost_materialized_header', get_string('generalheading', 'theme_boost_materialized'));
+
+//     // Logo file setting.
+//     $name = 'theme_boost_materialized/logo';
+//     $title = get_string('logo', 'theme_boost_materialized');
+//     $description = get_string('logodesc', 'theme_boost_materialized');
+//     $setting = new admin_setting_configstoredfile($name, $title, $description, 'logo');
+//     $setting->set_updatedcallback('theme_reset_all_caches');
+//     $temp->add($setting);
+
+//     // Custom CSS file.
+//     $name = 'theme_boost_materialized/customcss';
+//     $title = get_string('customcss', 'theme_boost_materialized');
+//     $description = get_string('customcssdesc', 'theme_boost_materialized');
+//     $default = '';
+//     $setting = new admin_setting_configtextarea($name, $title, $description, $default);
+//     $setting->set_updatedcallback('theme_reset_all_caches');
+//     $temp->add($setting);
+
+//     $settings->add($temp);
+
+//     /* Front Page Settings */
+//     $temp = new admin_settingpage('theme_boost_materialized_frontpage', get_string('frontpageheading', 'theme_boost_materialized'));
+
+//      // Who we are title.
+//     $name = 'theme_boost_materialized/whoweare_title';
+//     $title = get_string('whoweare_title', 'theme_boost_materialized');
+//     $description = '';
+//     $default = get_string('whoweare_title_default', 'theme_boost_materialized');
+//     $setting = new admin_setting_configtext($name, $title, $description, $default);
+//     $temp->add($setting);
+
+//      // Who we are content.
+//     $name = 'theme_boost_materialized/whoweare_description';
+//     $title = get_string('whoweare_description', 'theme_boost_materialized');
+//     $description = get_string('whowearedesc', 'theme_boost_materialized');
+//     $default = get_string('whowearedefault', 'theme_boost_materialized');
+//     $setting = new admin_setting_confightmleditor($name, $title, $description, $default);
+//     $temp->add($setting);
+
+//     $settings->add($temp);
+
+//     /* Slideshow Settings Start */
+//     $temp = new admin_settingpage('theme_boost_materialized_slideshow', get_string('slideshowheading', 'theme_boost_materialized'));
+//     $temp->add(new admin_setting_heading('theme_boost_materialized_slideshow', get_string('slideshowheadingsub', 'theme_boost_materialized'),
+//         format_text(get_string('slideshowdesc', 'theme_boost_materialized'), FORMAT_MARKDOWN)));
+
+//     // Display Slideshow.
+//     $name = 'theme_boost_materialized/toggleslideshow';
+//     $title = get_string('toggleslideshow', 'theme_boost_materialized');
+//     $description = get_string('toggleslideshowdesc', 'theme_boost_materialized');
+//     $yes = get_string('yes');
+//     $no = get_string('no');
+//     $default = 1;
+//     $choices = array(1 => $yes , 0 => $no);
+//     $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+//     $temp->add($setting);
+
+//     // Number of slides.
+//     $name = 'theme_boost_materialized/numberofslides';
+//     $title = get_string('numberofslides', 'theme_boost_materialized');
+//     $description = get_string('numberofslides_desc', 'theme_boost_materialized');
+//     $default = 1;
+//     $choices = array(
+//         1 => '1',
+//         2 => '2',
+//         3 => '3',
+//         4 => '4',
+//         5 => '5',
+//         6 => '6',
+//         7 => '7',
+//         8 => '8',
+//         9 => '9',
+//         10 => '10',
+//         11 => '11',
+//         12 => '12',
+//     );
+//     $temp->add(new admin_setting_configselect($name, $title, $description, $default, $choices));
+
+//     $numberofslides = get_config('theme_boost_materialized', 'numberofslides');
+//     for ($i = 1; $i <= $numberofslides; $i++) {
+
+//         // This is the descriptor for Slide One.
+//         $name = 'theme_boost_materialized/slide' . $i . 'info';
+//         $heading = get_string('slideno', 'theme_boost_materialized', array('slide' => $i));
+//         $information = get_string('slidenodesc', 'theme_boost_materialized', array('slide' => $i));
+//         $setting = new admin_setting_heading($name, $heading, $information);
+//         $temp->add($setting);
+
+//         // Slide Image.
+//         $name = 'theme_boost_materialized/slide' . $i . 'image';
+//         $title = get_string('slideimage', 'theme_boost_materialized');
+//         $description = get_string('slideimagedesc', 'theme_boost_materialized');
+//         $setting = new admin_setting_configstoredfile($name, $title, $description, 'slide' . $i . 'image');
+//         $setting->set_updatedcallback('theme_reset_all_caches');
+//         $temp->add($setting);
+
+//         // Slide Caption.
+//         $name = 'theme_boost_materialized/slide' . $i . 'caption';
+//         $title = get_string('slidecaption', 'theme_boost_materialized');
+//         $description = get_string('slidecaptiondesc', 'theme_boost_materialized');
+//         $default = get_string('slidecaptiondefault', 'theme_boost_materialized', array('slideno' => sprintf('%02d', $i) ));
+//         $setting = new admin_setting_configtext($name, $title, $description, $default, PARAM_TEXT);
+//         $temp->add($setting);
+
+//         // Slider button.
+//         $name = 'theme_boost_materialized/slide' . $i . 'urltext';
+//         $title = get_string('slidebutton', 'theme_boost_materialized');
+//         $description = get_string('slidebuttondesc', 'theme_boost_materialized');
+//         $default = 'lang:knowmore';
+//         $setting = new admin_setting_configtext($name, $title, $description, $default, PARAM_TEXT);
+//         $temp->add($setting);
+
+//         // Slide Description Text.
+//         $name = 'theme_boost_materialized/slide' . $i . 'url';
+//         $title = get_string('slideurl', 'theme_boost_materialized');
+//         $description = get_string('slideurldesc', 'theme_boost_materialized');
+//         $default = 'http://www.example.com/';
+//         $setting = new admin_setting_configtext($name, $title, $description, $default, PARAM_URL);
+//         $temp->add($setting);
+
+//     }
+
+//     /* Slideshow Settings End*/
+
+//     $settings->add($temp);
+
+//     /* Footer Settings start */
+//     $temp = new admin_settingpage('theme_boost_materialized_footer', get_string('footerheading', 'theme_boost_materialized'));
+
+//     // Footer Logo file setting.
+//     $name = 'theme_boost_materialized/footerlogo';
+//     $title = get_string('footerlogo', 'theme_boost_materialized');
+//     $description = get_string('footerlogodesc', 'theme_boost_materialized');
+//     $setting = new admin_setting_configstoredfile($name, $title, $description, 'footerlogo');
+//     $setting->set_updatedcallback('theme_reset_all_caches');
+//     $temp->add($setting);
+
+//     /* Footer Content */
+//     $name = 'theme_boost_materialized/footnote';
+//     $title = get_string('footnote', 'theme_boost_materialized');
+//     $description = get_string('footnotedesc', 'theme_boost_materialized');
+//     $default = get_string('footnotedefault', 'theme_boost_materialized');
+//     $setting = new admin_setting_confightmleditor($name, $title, $description, $default);
+//     $temp->add($setting);
+
+//     // INFO Link.
+//     $name = 'theme_boost_materialized/infolink';
+//     $title = get_string('infolink', 'theme_boost_materialized');
+//     $description = get_string('infolink_desc', 'theme_boost_materialized');
+//     $default = get_string('infolinkdefault', 'theme_boost_materialized');
+//     $setting = new admin_setting_configtextarea($name, $title, $description, $default);
+//     $temp->add($setting);
+
+//     // Copyright.
+//     $name = 'theme_boost_materialized/copyright_footer';
+//     $title = get_string('copyright_footer', 'theme_boost_materialized');
+//     $description = '';
+//     $default = get_string('copyright_default', 'theme_boost_materialized');
+//     $setting = new admin_setting_configtext($name, $title, $description, $default);
+//     $temp->add($setting);
+
+//     /* Address , Email , Phone No */
+//     $name = 'theme_boost_materialized/address';
+//     $title = get_string('address', 'theme_boost_materialized');
+//     $description = '';
+//     $default = get_string('defaultaddress', 'theme_boost_materialized');
+//     $setting = new admin_setting_configtext($name, $title, $description, $default);
+//     $temp->add($setting);
+
+//     $name = 'theme_boost_materialized/emailid';
+//     $title = get_string('emailid', 'theme_boost_materialized');
+//     $description = '';
+//     $default = get_string('defaultemailid', 'theme_boost_materialized');
+//     $setting = new admin_setting_configtext($name, $title, $description, $default);
+//     $temp->add($setting);
+
+//     $name = 'theme_boost_materialized/phoneno';
+//     $title = get_string('phoneno', 'theme_boost_materialized');
+//     $description = '';
+//     $default = get_string('defaultphoneno', 'theme_boost_materialized');
+//     $setting = new admin_setting_configtext($name, $title, $description, $default);
+//     $temp->add($setting);
+
+//     /* Facebook, Pinterest, Twitter, Google+ Settings */
+//     $name = 'theme_boost_materialized/fburl';
+//     $title = get_string('fburl', 'theme_boost_materialized');
+//     $description = get_string('fburldesc', 'theme_boost_materialized');
+//     $default = get_string('fburl_default', 'theme_boost_materialized');
+//     $setting = new admin_setting_configtext($name, $title, $description, $default);
+//     $temp->add($setting);
+
+//     $name = 'theme_boost_materialized/pinurl';
+//     $title = get_string('pinurl', 'theme_boost_materialized');
+//     $description = get_string('pinurldesc', 'theme_boost_materialized');
+//     $default = get_string('pinurl_default', 'theme_boost_materialized');
+//     $setting = new admin_setting_configtext($name, $title, $description, $default);
+//     $temp->add($setting);
+
+//     $name = 'theme_boost_materialized/twurl';
+//     $title = get_string('twurl', 'theme_boost_materialized');
+//     $description = get_string('twurldesc', 'theme_boost_materialized');
+//     $default = get_string('twurl_default', 'theme_boost_materialized');
+//     $setting = new admin_setting_configtext($name, $title, $description, $default);
+//     $temp->add($setting);
+
+//     $name = 'theme_boost_materialized/gpurl';
+//     $title = get_string('gpurl', 'theme_boost_materialized');
+//     $description = get_string('gpurldesc', 'theme_boost_materialized');
+//     $default = get_string('gpurl_default', 'theme_boost_materialized');
+//     $setting = new admin_setting_configtext($name, $title, $description, $default);
+//     $temp->add($setting);
+
+//     $settings->add($temp);
+//     /*  Footer Settings end */
+// }
